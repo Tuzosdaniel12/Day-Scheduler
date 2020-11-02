1cc4865b440fdcd76dbbee8e2079178fbe3080de
daniel <danielsoledad@gmail.com>
2020-11-02 01:47:05 -0800

    adding local storage to application, i have succesfully set the data but not able to get it back

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -9,16 +9,15 @@
 var timeBLock = $('.time-block');
 var currentDayEl =$('#currentDay')
 
-
+var textAreaElList = [];
 var hoursList = [9,10,11,12,1,2,3,4,5];
 var dataWords= ["zero","one","two","three","four","five","six","seven","eight","nine"];
 var currentDay= moment();
 var currentHour = moment().format("h");
 currentDayEl.text(currentDay.format("dddd, MMMM Do YYYY"));
-var schedule = []
+var textAreaValue = [];
 var armyTime = 12;
 //this function creates, elements for each row in the schedule 
-
     for(var i =0; i < hoursList.length; i++){
        // console.log(hoursList);
         var row = $('<div>').addClass('row').attr('data-index',i);//create each row
@@ -38,85 +37,139 @@ var armyTime = 12;
             hourDiv.attr('data-index', armyTime);
             armyTime++;
         }
-        
-        console.log(hourDiv.attr('data-index'));
+        //console.log(textArea.attr('id'));
         row.append(hourDiv, textArea, saveBtn);
         saveBtn.append(icon);
+        textAreaElList.push({textAreaEl : $('#'+dataWords[i])}); 
         //console.log(saveBtn);
     }
 
     
-    
+    console.log(textAreaElList);
     function savaInfo(event){
         event.preventDefault();
         console.log();
         var index =parseInt( $(event.target).attr('data-index'));//grab button dataindex and call a function to save text content matching data -index
         console.log(index);
-        switch(index){
+        switch(index){//when they hit the save button they will send the right text box text to save to local storage
             case 0:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
-                console.log(string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
                 break;
             case 1:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
-                console.log(string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
+                
+                //console.log(string);
                 break;
             case 2:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
-                console.log(string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
+
+                //console.log(string);
                 break;
             case 3:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
+
                 console.log(string);
                 break;
             case 4:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
+
                 console.log(string);
                 break;
             case 5:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
+
                 console.log(string);
                 break;
             case 6:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
+
                 console.log(string);
                 break;
             case 7:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
-                console.log(string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                textAreaValue = saveData(textAreaValue);
+                renderData(textAreaValue);
+
                 break;
             case 8:
                 var string = $(event.target.parentElement.children[1]).val();
-                saveData(index, string);
-                console.log(string);
+                var textareaId = textAreaElList[index].textAreaEl.attr('id');
+                textAreaValue.push({textareaId:textareaId, savedText:string})
+                renderData(textAreaValue);
+                textAreaValue = saveData(textAreaValue);
+                console.log(textAreaValue);
                 break;
         }
 }
-function saveData(index, string){
-    var schedule = [" "," "," "," "," "," "," "," "," "];
-    console.log(schedule.length);
-    
-    if(JSON.parse(localStorage.getItem("schedule")) === undefined){
-        schedule[index] = string;
-    }
-    else{
-        schedule = JSON.parse(localStorage.getItem("schedule"));
-    }
-    schedule[index] = string;
-    localStorage.setItem("schedule", JSON.stringify(schedule));
-    schedule = JSON.parse(localStorage.getItem("schedule"));
+function saveData(array){///this function takes the id of text area and the text that was saved and is stored in array of objects so latter i can fetch it from local storage
+    localStorage.setItem("array", JSON.stringify(array));
+    array = JSON.parse(localStorage.getItem("array"));
+    return array;
+}
 
-    for(var i =0; i < schedule.length; i++){
-        timeBLock.children().eq(i).children().eq(1).text(schedule[i]);
+
+function renderData(array){
+    for(var i = 0; i < array.length;i++){
+        switch(array[i].textareaId){
+            case "zero":
+                textAreaElList[0].textAreaEl.text(array[i].savedText);
+                break;
+            case "one":
+                textAreaElList[1].text(array[i].savedText);
+                break;
+            case "two":
+                textAreaElList[2].text(array[i].savedText);
+                break;
+            case "three":
+                textAreaElList[3].text(array[i].savedText);
+                break; 
+            case "four":
+                textAreaElList[4].text(array[i].savedText);
+                break;
+            case "five":
+                textAreaElList[5].text(array[i].savedText);
+                break;
+            case "six":
+                textAreaElList[6].text(array[i].savedText);
+                break;
+            case "seven":
+                textAreaElList[7].text(array[i].savedText);
+                break;
+            case "eight":
+                textAreaElList[8].text(array[i].savedText);
+                break;   
+        }
     }
 }
 
-timeBLock.on('click', '.saveBtn', savaInfo);///delagate funtion for all save buttons
+timeBLock.on('click', '.saveBtn', savaInfo);//delagate funtion for all save buttons
